6612c06fe9ef9b5ed961c1a97a58c8e0a797cb69
Kohei Iwanaga <iwanaga@studiorex.co.jp>
2020-04-24 19:04:45 +0900

    Bug fix

--- lib/fix_tsv_conflict/repairman.rb
+++ lib/fix_tsv_conflict/repairman.rb
@@ -22,27 +22,73 @@ module FixTSVConflict
       branch = nil
       left,  lbranch = [], nil
       right, rbranch = [], nil
+      merged_line,  _merged_line = nil, nil
+      lmerged_line, rmerged_line = nil, nil
 
-      source.each_line.with_index do |line, i|
+      source_lines = source.each_line.to_a
+      source_lines.each_with_index do |line, i|
         if i.zero?
           load_tabs_count(line)
           result << line
         elsif line.start_with?(LEFT)
           lbranch = line.chomp.split(" ").last
           branch = left
+          _merged_line = merged_line
         elsif line.start_with?(SEP)
+          lmerged_line = merged_line
           branch = right
+          merged_line = _merged_line
         elsif line.start_with?(RIGHT)
+          rmerged_line = merged_line
+          if lmerged_line && rmerged_line
+            while (next_line = source_lines[i + 1])
+              lmerged_line += next_line
+              rmerged_line += next_line
+              source_lines.delete_at(i + 1)
+              break if next_line.scan(QUOTE).size.odd?
+            end
+            left  << lmerged_line
+            right << rmerged_line
+          end
           rbranch = line.chomp.split(" ").last
           result += handle(left, lbranch, right, rbranch)
           branch = nil
           left.clear
           right.clear
+          merged_line = nil
         else
           if branch
-            branch << line
+            if line.scan(QUOTE).size.odd?
+              if merged_line.nil?
+                merged_line = line
+              else
+                merged_line += line
+                branch << merged_line
+                merged_line = nil
+              end
+            else
+              if merged_line.nil?
+                branch << line
+              else
+                merged_line += line
+              end
+            end
           else
-            result << line
+            if line.scan(QUOTE).size.odd?
+              if merged_line.nil?
+                merged_line = line
+              else
+                merged_line += line
+                result << merged_line
+                merged_line = nil
+              end
+            else
+              if merged_line.nil?
+                result << line
+              else
+                merged_line += line
+              end
+            end
           end
         end
       end
